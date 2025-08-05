b605f1578343eb2a32cccfae1a60d3391fe60ac8
Alex Balter <albalter@gmail.com>
2025-08-05 17:21:43 +0300

    Add logic for adjudicating points in each round played

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+let roundsPlayed =0;
 
 function getComputerChoice() {
     choice = Math.floor(Math.random()*3)+1;
@@ -9,4 +11,37 @@ function getComputerChoice() {
     }
 };
 
-console.log(getComputerChoice());
+function getHumanChoice () {
+   return prompt ("Please enter your move (rock, paper or scissors)").toLowerCase;
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice();
+
+function playRound(computerChoice, humanChoice) {
+  if (humanSelection !== computerSelection){
+    if (humanSelection ==="rock" && computerSelection==="paper") {
+        computerScore += 1;
+    } else if (humanSelection === "rock" && computerSelection==="scissors"){
+        humanScore += 1;
+    }
+        
+    if (humanSelection ==="scissors" && computerSelection ==="paper"){
+        humanScore+=1
+    } else if (humanSelection === "scissors" && computerSelection==="rock") {
+        computerScore +=1
+    }
+
+    if (humanSelection ==="paper" && computerSelection ==="rock"){
+        humanScore += 1
+    } else is (humanSelection==="paper" && computerSelection==="scissors"){
+        computerScore +=1
+    }
+  }
+  roundsPlayed+=1;
+    
+}
+
+
+console.log(`computer's move: ${computerSelection}`);
+console.log(`Your move: ${humanSelection}`)
